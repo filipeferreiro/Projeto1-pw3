c9bd15e7e3c825df8b04b8bd495fe238f98a7872
Leandro Moura <leofcb334@gmail.com>
2022-03-21 10:39:23 -0300

    commit

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -1,42 +1,60 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
+using System.Configuration;
 using System.Data;
+
 
 namespace Projeto1
 {
-    class Banco
+    class Banco : IDisposable
     {
-        private readonly MySqlConnection conexao;
+        private readonly MySqlConnection conexao; 
         public Banco()
         {
             
-            MySqlConnection conexao = new MySqlConnection("Server=localhost;DataBase=dbExemplo; User=root; pwd=12345678");
+            conexao = new MySqlConnection(ConfigurationManager.ConnectionStrings["conexao"].ConnectionString);
             conexao.Open();
+    
 
-            
         }
+
+       
+        
         public void ExecutaComando (string StrQuery)
         {
             var vComando = new MySqlCommand
             {
-                CommandText = StrQuery, CommandType = System.Data.CommandType.Text, Connection = conexao
+                CommandText = StrQuery, CommandType = CommandType.Text, Connection = conexao
             };
+            vComando.ExecuteNonQuery();
         }
+         public MySqlDataReader RetornaComando (string StrQuery)
+        {
+            var comando = new MySqlCommand(StrQuery, conexao);
+            return comando.ExecuteReader(); 
+        }
+
+        public void Dispose()
+        {
+            if(conexao.State==ConnectionState.Open)
+            conexao.Close();
+        }
+
     }
      
-    
     class Program
     {
         static void Main(string[] args)
         {
+            var Banco = new Banco();
             
-
-            /* string strAtualizaUsu = "update tbUsuario set nomeUsu = 'meachoesperta' where idUsu = 2 ";
+            /*
+             string strAtualizaUsu = "update tbUsuario set nomeUsu = 'meachoesperta' where idUsu = 2 ";
              MySqlCommand comandoAtualiza = new MySqlCommand(strAtualizaUsu, conexao);
              comandoAtualiza.ExecuteNonQuery();
-            */
+            
 
-            /*
+            
               string insereUsu = "insert into tbUsuario(nomeUsu, cargo, datanasc) values('Emma', 'cerimonialista', '2000/04/17')";
               MySqlCommand comandoinserir = new MySqlCommand(insereUsu, conexao);
               comandoinserir.ExecuteNonQuery();*/
@@ -49,40 +67,44 @@ namespace Projeto1
             string vData = Console.ReadLine();
 
 
-            string strinsereUsu = string.Format("insert into tbUsuario(nomeUsu, cargo, datanasc) VALUES('{0}', '{1}', DATE_FORMAT(STR_TO_DATE('{2}', '%d/%m/%Y'), '%Y-%m-%d'));", vNome, vCargo, vData);
+            var usuario = new Usuario { nomeUsu = vNome, cargo = vCargo, datanasc = DateTime.Parse(vData) }; // erro de conversao.
+            new UsuarioDAO().Atualizar(usuario);   
 
-            MySqlCommand comandoinserir = new MySqlCommand(strinsereUsu, conexao);
-            comandoinserir.ExecuteNonQuery();
-
-         
-            
-
-
-
-
-          /* MySqlCommand comandoApagar = new MySqlCommand("delete from tbUsuario where idUsu = 2", conexao);
-            comandoApagar.ExecuteNonQuery();*/
 
             string strSelecionaUsu = "Select * from tbUsuario";
-            MySqlCommand comando = new MySqlCommand(strSelecionaUsu, conexao);
-            MySqlDataReader leitor = comando.ExecuteReader();
+            MySqlDataReader leitor = Banco.RetornaComando(strSelecionaUsu);
+
 
             while (leitor.Read())
             {
                 Console.WriteLine("Id: {0}, Nome: {1}, Cargo: {2}, Data: {3}", leitor["idUsu"], leitor["nomeUsu"], leitor["cargo"], leitor["datanasc"]);
             };
 
-
-
-           
-
-
-
-
-         
-
-
             Console.ReadLine();
+
+
+
+            /*  MySqlCommand comandoinserir = new MySqlCommand(strinsereUsu, conexao);
+              comandoinserir.ExecuteNonQuery();
+
+
+
+
+
+
+
+             MySqlCommand comandoApagar = new MySqlCommand("delete from tbUsuario where idUsu = 2", conexao);
+              comandoApagar.ExecuteNonQuery();
+
+
+
+              while (leitor.Read())
+              {
+                  Console.WriteLine("Id: {0}, Nome: {1}, Cargo: {2}, Data: {3}", leitor["idUsu"], leitor["nomeUsu"], leitor["cargo"], leitor["datanasc"]);
+              };
+
+
+              Console.ReadLine(); */
         }
     }
 }
---
+++ Projeto1/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto1
+{
+    class Usuario
+    {
+        public int idUsu {get; set; }
+        public string nomeUsu { get; set; }
+
+        public string cargo { get; set; }
+
+        public DateTime datanasc { get; set; }
+
+
+        
+    }
+}
